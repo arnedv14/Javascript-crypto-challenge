78efa0fad3cdb3e0cfd1b141e89c21997141d063
arnedv14 <arne.de.vroede@student.ehb.be>
2020-11-01 21:07:29 +0100

    code clean-up

--- src/SecureSessionPeer.js
+++ src/SecureSessionPeer.js
@@ -1,68 +1,59 @@
 const nacl = require("libsodium-wrappers");
-const Decryptor=require('./Decryptor.js');
-const Encryptor=require('./Encryptor.js');
+const Decryptor = require("./Decryptor.js");
+const Encryptor = require("./Encryptor.js");
 
-module.exports = async (peer) => {
-  
+module.exports = async (otherPeer) => {
   await nacl.ready;
   //generate public key and private key
   let keyPair = nacl.crypto_kx_keypair();
+  //create a global variable 'message' used to exchange messages
   global.message;
-  let [pk, sk] = [keyPair.publicKey, keyPair.privateKey];
-  let encryptor,decryptor;  
-  if(peer == null){
-    //Return a client object
-    
-    let clientObj={
-      publicKey:pk,
-      connection:null,
-      encrypt: (msg)=>{
-        return encryptor.encrypt(msg);
-      },
-      decrypt: (ciphertext, nonce)=>{
-        return decryptor.decrypt(ciphertext,nonce);
-      },
-      send:(msg)=>{
-        message=encryptor.encrypt(msg);
-      },
-      receive:(msg)=>{
-        return decryptor.decrypt(message.ciphertext,message.nonce);
-      },
-      connect:async (peer)=>{
-        this.connection=peer;
-        let keysClient= nacl.crypto_kx_client_session_keys(pk,sk,peer.publicKey);
-        encryptor=await Encryptor(keysClient.sharedTx);
-        decryptor=await Decryptor(keysClient.sharedRx);
-      }
-      
-    }
-    
-    
-    return Object.freeze(clientObj);
-  }else{
-    let keysServer = nacl.crypto_kx_server_session_keys(pk,sk,peer.publicKey);
-    const decryptor=await Decryptor(keysServer.sharedRx);
-    const encryptor=await Encryptor(keysServer.sharedTx);
-    //return a server object
-    let serverObj={
-      publicKey:pk,
-      encryptionKey:keysServer.sharedTx,
-      decryptionKey:keysServer.sharedRx,
-      encrypt:(msg)=>{
-        return encryptor.encrypt(msg);
-      },
-      decrypt:(ciphertext, nonce)=>{
-        return decryptor.decrypt(ciphertext,nonce);
-      },
-      send:(msg)=>{
-        message=encryptor.encrypt(msg);
-      },
-      receive:(msg)=>{
-        return decryptor.decrypt(message.ciphertext,message.nonce);
-      }
-    }
 
-    peer.connect(serverObj);
-    return Object.freeze(serverObj);
-  }   
-}
+  //create variables to store the instances of encryptor and decryptor once an otherPeer is defined
+  let encryptor, decryptor;
+
+  //create object to return
+  let peerObject = {
+    publicKey: keyPair.publicKey,
+    encrypt: (msg) => {
+      return encryptor.encrypt(msg);
+    },
+    decrypt: (ciphertext, nonce) => {
+      return decryptor.decrypt(ciphertext, nonce);
+    },
+    send: (msg) => {
+      message = encryptor.encrypt(msg);
+    },
+    receive: () => {
+      return decryptor.decrypt(message.ciphertext, message.nonce);
+    },
+    connect: async (otherPeer) => {
+      let keysClient = nacl.crypto_kx_client_session_keys(
+        keyPair.publicKey,
+        keyPair.privateKey,
+        otherPeer.publicKey
+      );
+      encryptor = await Encryptor(keysClient.sharedTx);
+      decryptor = await Decryptor(keysClient.sharedRx);
+    },
+  };
+
+  Object.freeze(peerObject);
+
+  //Set encryption- and decrpytion keys when the peer is a server-instance
+  if (otherPeer != null) {
+    let keysServer = nacl.crypto_kx_server_session_keys(
+      keyPair.publicKey,
+      keyPair.privateKey,
+      otherPeer.publicKey
+    );
+
+    decryptor = await Decryptor(keysServer.sharedRx);
+    encryptor = await Encryptor(keysServer.sharedTx);
+
+    //initialize encryption- and decryption keys in client-instance
+    otherPeer.connect(peerObject);
+  }
+
+  return peerObject;
+};
